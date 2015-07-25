d657d141b6be3deea00a61a507d746294d64bd53
absharma3 <master.abhimanyu@gmail.com>
2015-07-25 14:03:55 +0530

    Initial Commit

--- Backend/DestinationHack/src/com/apps/insideout/DestHackFindPreferences.java
+++ Backend/DestinationHack/src/com/apps/insideout/DestHackFindPreferences.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class AccomodationMate
  */
 @WebServlet("/AccomodationMate")
-public class AccomodationMate extends HttpServlet {
+public class DestHackFindPreferences extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AccomodationMate() {
+    public DestHackFindPreferences() {
         super();
         // TODO Auto-generated constructor stub
     }
